bfcfe7ed24877aaaca904ab062c01d36933ea71d
Gilberto <gilberto_silva.r@hotmail.com>
2022-04-11 15:10:41 -0300

    my first commit about a bank account project

---
+++ Course/Course/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class BankAccount
+    {
+        public int Number { get; private set; }
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+
+        public BankAccount(int number, string holder)
+        {
+            Number = number;
+            Holder = holder;
+        }
+
+        public BankAccount(int number, string holder, double initialDeposit) : this(number, holder)
+        {
+            Deposit(initialDeposit);
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            Balance -= amount + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Account "
+                + Number
+                + ", Holder: "
+                + Holder
+                + ", Withdraw: $ "
+                + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Course/Course/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account;
+
+            Console.Write("Enter account number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Account's Owner Name: ");
+            string holder = Console.ReadLine();
+
+            Console.Write("Will there be an initial deposit?(y/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if(resp == 'y' || resp == 'Y')
+            {
+                Console.Write("Enter the initial deposit amount: ");
+                double initialDeposit= double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account = new BankAccount(number, holder, initialDeposit);
+            }
+            else
+            {
+                account = new BankAccount(number, holder);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Bank Account Details: ");
+            Console.WriteLine(account);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter a Deposit Amount: ");
+            double amount = double.Parse(Console.ReadLine());
+            account.Deposit(amount);
+
+            Console.WriteLine();
+            Console.WriteLine("Updated Bank Account Details: ");
+            Console.WriteLine(account);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter a Withdrawal Amount: ");
+            Console.WriteLine("Note: Withdrawals have 5% discount...");
+            double withDraw = double.Parse(Console.ReadLine());
+            account.WithDraw(withDraw);
+
+            Console.WriteLine();
+            Console.WriteLine("Updated Bank Account Details: ");
+            Console.WriteLine(account);
+
+        }
+    }
+}
